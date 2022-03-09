bf3ab04a82596775bd1d8401d193d38f8567dd4f
Damian Jacob <damianm.jacob@yahoo.com>
2022-03-09 16:07:23 +0000

    create index.html template by extending base.html

--- breadit/forum/views.py
+++ breadit/forum/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'base.html')
+    post_list = Post.objects.order_by('-created_on')
+    context = {'post_list': post_list}
+    return render(request, 'index.html', context)
