14bc804dffaae7f256d4e0cd35f8361a8c2ad85a
Damian Jacob <damianm.jacob@yahoo.com>
2022-03-11 10:35:45 +0000

    Change function view to class-based view in forum/views.py

--- breadit/forum/urls.py
+++ breadit/forum/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+from .views import PostView
+
 urlpatterns = [
-    path('', views.index, name='index')
+    # path('', views.index, name='index'),
+
+    # class based view path:
+    path('', PostView.as_view()),
 ]
--- breadit/forum/views.py
+++ breadit/forum/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
 
-def index(request):
-    post_list = Post.objects.order_by('-created_on')
-    context = {'post_list': post_list}
-    return render(request, 'index.html', context)
+# def index(request):
+#     post_list = Post.objects.order_by('-created_on')
+#     context = {'post_list': post_list}
+#     return render(request, 'index.html', context)
+
+# Creating the class based view for index
+class PostView(ListView):
+    model = Post
+    queryset = Post.objects.order_by('-created_on')
+    post_list = queryset
+    template_name = 'index.html'
