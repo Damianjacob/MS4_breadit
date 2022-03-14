7f2cadb7b8b2b2d6894e84f5f630bff55cf3d254
Damian Jacob <damianm.jacob@yahoo.com>
2022-03-14 09:41:58 +0000

    Add login page to templates

--- breadit/forum/urls.py
+++ breadit/forum/urls.py
@@ -4,9 +4,6 @@ from . import views
 from .views import PostView, PostDetailView
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-
-    # class based view path:
     path('', PostView.as_view(), name='index'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
