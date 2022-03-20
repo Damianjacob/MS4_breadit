36de7fe2be693dfa0001b974aa8a72de1c0e9091
Damian Jacob <damianm.jacob@yahoo.com>
2022-03-20 13:16:57 +0000

    update requirements.txt and settings.py

--- breadit/breadit/settings.py
+++ breadit/breadit/settings.py
@@ -33,12 +33,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-# ALLOWED_HOSTS = ['breadit-app.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['breadit-app.herokuapp.com', 'localhost']
 
 
 # Application definition
